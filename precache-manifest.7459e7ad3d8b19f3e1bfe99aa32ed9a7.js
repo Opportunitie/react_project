613d8e4811907ce2b74d07707ffe6b00cada03f4
25524 <tao.wang13@hand-china.com>
2019-07-23 19:01:59 +0800

    07-22react作业

---
+++ precache-manifest.7459e7ad3d8b19f3e1bfe99aa32ed9a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4feb56feb33de7b11cbfd55403b232b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87df45f5b4c63004b596",
+    "url": "/static/css/2.56c6e76d.chunk.css"
+  },
+  {
+    "revision": "0e42d5bd0fb4b50c054f",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "87df45f5b4c63004b596",
+    "url": "/static/js/2.e5a50ef8.chunk.js"
+  },
+  {
+    "revision": "0e42d5bd0fb4b50c054f",
+    "url": "/static/js/main.ccf5e06d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
